38b6046cbc68d959a78862d7bbe1a5fc9f3eba67
PelusaCosmica <158301711+PelusaCosmica@users.noreply.github.com>
2024-08-28 07:53:20 -0600

    Update Prestamo.cpp

--- Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Prestamo.cpp
+++ Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Prestamo.cpp
@@ -11,3 +11,9 @@ void Prestamo::VerDatosPrestamo() {
 	cout << "El interes del prestamo es: " << Prestamo::TazaI << endl;
 	cout << "El plazo para pagar el prestamo es de :" << Prestamo::Plazo << endl;
 }
+void Prestamo::setMonto(double monto) { Prestamo::Monto = monto; }
+void Prestamo::setTazaI(double tazai) { Prestamo::TazaI = tazai; }
+void Prestamo::setPlazo(int plazo) { Prestamo::Plazo = Plazo; }
+double Prestamo::getMonto() { return Prestamo::Monto; }
+double Prestamo::getTazaI() { return Prestamo::TazaI; }
+int Prestamo::getPlazo() { return Prestamo::Plazo; }
