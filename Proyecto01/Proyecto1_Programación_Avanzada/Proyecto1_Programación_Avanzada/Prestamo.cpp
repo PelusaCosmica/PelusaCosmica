ae0bcc78c789715a7146d138249a3127248cce1f
PelusaCosmica <158301711+PelusaCosmica@users.noreply.github.com>
2024-08-28 07:42:02 -0600

    Update Prestamo.cpp

--- Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Prestamo.cpp
+++ Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Prestamo.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 #include "Prestamo.h"
+
+using namespace std;
+
+double Prestamo::CalculoInteres() {
+	cout << "Calculo de interes según tipo de prestamo" << endl;
+}
+void Prestamo::VerDatosPrestamo() {
+	cout << "El monto actual del prestamo es de: " << Prestamo::Monto << endl;
+	cout << "El interes del prestamo es: " << Prestamo::TazaI << endl;
+	cout << "El plazo para pagar el prestamo es de :" << Prestamo::Plazo << endl;
+}
