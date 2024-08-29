838b7c30af9cca224d4eb547a46ead4c65ce5181
Ana Paula Ortiz <158302126+4paula0r@users.noreply.github.com>
2024-08-28 20:03:33 -0600

    Hipotecario.cpp

--- Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Hipotecario.cpp
+++ Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Hipotecario.cpp
@@ -1,1 +1,37 @@
 #include "Hipotecario.h"
+
+
+using namespace std;
+#include <iostream>
+
+
+Hipotecario::Hipotecario(double monto, double tasai, int plazo, double euribor, double diferencial, double capitalpendiente) : Prestamo(monto, tasai, plazo) {
+
+    Monto = monto;
+    TasaInteres = tasai;
+    Plazo = plazo;
+    Euribor = euribor;
+    Diferencial = diferencial;
+    CapitalPendiente = capitalpendiente;
+}
+
+double Hipotecario::CalculoIntereses()   {
+    
+
+    return  CapitalPendiente * (Euribor + Diferencial) / 12;
+
+}
+
+    void  Hipotecario::VerDatosPrestamo()  {
+
+    cout << "El prestamo Hipotecario: " << endl;
+    cout << "El capital pendiente es: Q." << CapitalPendiente << endl;
+    cout << "El Euribor: " << Euribor << endl;
+    cout << "El interes mensuale es: " << CalculoIntereses() << endl;
+
+}
+
+void Hipotecario::Abonar(double cantabonada) {
+
+    CapitalPendiente -=cantabonada;
+}
