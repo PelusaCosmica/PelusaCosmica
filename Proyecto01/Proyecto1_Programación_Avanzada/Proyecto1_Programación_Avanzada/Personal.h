f52c9cd41658b352b2ddd6a4e1dbf0c3a48d1345
Ana Paula Ortiz <158302126+4paula0r@users.noreply.github.com>
2024-08-28 19:57:00 -0600

    Update Personal.h

--- Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Personal.h
+++ Proyecto01/Proyecto1_Programación_Avanzada/Proyecto1_Programación_Avanzada/Personal.h
@@ -3,5 +3,30 @@
 class Personal :
     public Prestamo
 {
+
+private:
+    float i = 0.0645;
+    double P;
+    int NumDias;
+   
+
+public: 
+    double Monto;
+    double TasaInteres;
+    int Plazo;
+
+
+    Personal::Personal(double monto, double tasai, int plazo, float i,
+    double P, int n; ): Prestamo(monto, tasai, plazo) {
+
+        Monto = monto;
+        TasaInteres = tasai;
+        Plazo = plazo;
+        i = i;
+        P = P;
+        NumDias = n;
+  
+    }
+     
 };
 
